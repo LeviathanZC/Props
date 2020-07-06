56a7ab6883828d79d6d1581919db13b5f4915ef6
LeviathanZC <leviathan.zercomp@gmail.com>
2020-07-06 23:21:43 +0300

    Finish-finish.

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -1,28 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class MainController {
 
-    public static void main(String[] args) {
+    private static final String BELARUS = "BY";
+    private static final String RUSSIAN = "RU";
+
+    public static void main(final String[] args) throws IOException {
         System.out.println("1-Default, 2-Russian");
-        String input = "2";
+        int input = readNumber();
         String country = "";
         String lang = "";
-        if(input.equals("2")) {
-            country = "BY";
-            lang = "RU";
+        if(input == 2) {
+            country = BELARUS;
+            lang = RUSSIAN;
         }
-        Locale locale = new Locale(lang, country);
-        ResourceBundle resources = ResourceBundle.getBundle("property.text", locale);
-        String comma = ", ";
-        String first = resources.getString("first");
-        String second = resources.getString("second");
-        String third = resources.getString("third");
+        Locale current = new Locale(lang, country);
+        ResourceBundle resources = ResourceBundle.getBundle("property.text", current);
+        final String comma = ", ";
+        final String first = resources.getString("first");
+        final String second = resources.getString("second");
+        final String third = resources.getString("third");
         System.out.println(new StringBuilder(first)
                                 .append(comma)
                                 .append(second)
                                 .append(comma)
                                 .append(third));
+    }
+
+    private static int readNumber() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return Integer.parseInt(reader.readLine());
     }
 
 }
